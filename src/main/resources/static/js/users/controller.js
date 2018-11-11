4126cc7d229e731a88f3258c3c5f214b454d2186
vit <vit.blokhin@gmail.com>
2018-11-11 17:46:45 +0300

    fix:user info edit

--- src/main/resources/static/js/users/controller.js
+++ src/main/resources/static/js/users/controller.js
@@ -6,10 +6,10 @@ UserController = {};
 
 UserController.edit = function(){
     //TODO
-    sendRequest('#user-form', null, showSuccess);
+    sendRequest('#user-form', null, null, showSuccess);
 };
 
 UserController.password = function(){
     //TODO
-    sendRequest('#password-form', null, showSuccess);
+    sendRequest('#password-form', null, null, showSuccess);
 };
